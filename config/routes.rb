b6da70b3b9d95e290d9db311b12d4f3c730633db
Naoko Tashiro <108598161+yuzu1117@users.noreply.github.com>
2022-07-16 08:08:11 +0000

    Done all

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'homes/top' => 'homes#top'
+  get 'homes/top'=>'homes#top'
+  root to: 'homes#top'
   resources :books
- 
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
