cbf7de125d7a56e00a103c87eb7428175ce38ce5
Naoko Tashiro <108598161+yuzu1117@users.noreply.github.com>
2022-07-16 06:46:58 +0000

    finish html

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "ccec253cae2f48e89a984dbe418f5f47.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
